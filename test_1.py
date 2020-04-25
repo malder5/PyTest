8c6ab073b0800bf7c6540867e5e0a4e62e54b433
Roman <mulyarchik.rk@yandex.ru>
2020-04-25 18:23:57 +0300

    Вынесли тестовые методы в отдельный класс. Создали фикстуру. Перешли от фреймворка unittest к Pytest.

--- test_1.py
+++ test_1.py
@@ -1,83 +1,37 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 import unittest
+import pytest
 import time
 from group import group
+from application import Application
 
 
-class test_add_group(unittest.TestCase):
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+class test_add_groups(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver(executable_path=r'C:\Users\Roman\Downloads\chromedriver.exe')
-        self.wd.implicitly_wait(60)
+        self.app = Application
 
     def test_test_add_empty_group(self):
-        success = True
-        wd = self.wd
-        self.open_home_page(wd)
-
-        self.login(wd, user='admin', password='secret')
-
-        self.create_group(wd, group(name='', header='', footer=''))
-        time.sleep(1)
-        self.return_group_page(wd)
-        self.logout(wd)
+        wd = self.app.wd
+        self.app.login(user='admin', password='secret')
+        self.app.create_group(group(name='', header='', footer=''))
+        self.app.logout()
 
     def test_test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-
-        self.login(wd, user='admin', password='secret')
-
-        self.create_group(wd, group(name='ljfsalrekj', header=';lkj', footer='lkjh'))
+        wd = self.app.wd
+        self.app.login(user='admin', password='secret')
+        self.app.create_group(group(name='ljfsalrekj', header=';lkj', footer='lkjh'))
         time.sleep(1)
-        self.return_group_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-
-        # logout
-        wd.find_element_by_link_text('Logout').click()
-
-    def return_group_page(self, wd):
-        # Return to groups page
-        wd.find_element_by_link_text('groups').click()
-
-    def create_group(self, wd, group):
-
-        # create group
-        wd.find_element_by_link_text('groups').click()
-
-        # Init_group creation
-        wd.find_element_by_name('new').click()
-
-        # fill group form
-        wd.find_element_by_name('group_name').click()
-        wd.find_element_by_name('group_name').clear()
-        wd.find_element_by_name('group_name').send_keys('%s' % group.name)
-        wd.find_element_by_name('group_header').click()
-        wd.find_element_by_name('group_header').clear()
-        wd.find_element_by_name('group_header').send_keys('%s' % group.header)
-        wd.find_element_by_name('group_footer').click()
-        wd.find_element_by_name('group_footer').clear()
-        wd.find_element_by_name('group_footer').send_keys('%s' % group.footer)
-        # Subimt group creation
-        wd.find_element_by_name('submit').click()
-
-    def login(self, wd, user, password):
-        # Login page
-        wd.find_element_by_name('user').click()
-        wd.find_element_by_name('user').clear()
-        wd.find_element_by_name('user').send_keys('%s' % user)
-        wd.find_element_by_name('pass').click()
-        wd.find_element_by_name('pass').clear()
-        wd.find_element_by_name('pass').send_keys('%s' % password)
-        wd.find_element_by_css_selector('input[type=\"submit\"]').click()
-
-    def open_home_page(self, wd):
-        # open web-page
-        wd.get('http://localhost/addressbook')
+        self.app.logout()
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 
 if __name__ == '__main__':
