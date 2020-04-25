dfd7fd679cc79ece47fab5a9ba03c8525e4f1996
Roman <mulyarchik.rk@yandex.ru>
2020-04-25 21:01:17 +0300

    Добавлен метод и тест к удалению контакта

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -4,10 +4,35 @@ class ContactsHelper:
     def __init__(self, app):
         self.app = app
 
-    def create(self):
+    def create(self, Contacts):
         wd = self.app.wd
         wd.find_element_by_css_selector('#nav > ul > li:nth-child(2) > a').click()
-        # wd.find_element_by_link_text('add_new').click()
+
+        wd.find_element_by_name('firstname').click()
+        wd.find_element_by_name('firstname').clear()
+        wd.find_element_by_name('firstname').send_keys(Contacts.firstname)
+
+        wd.find_element_by_name('lastname').click()
+        wd.find_element_by_name('lastname').clear()
+        wd.find_element_by_name('lastname').send_keys(Contacts.lastname)
+
+        wd.find_element_by_name('home').click()
+        wd.find_element_by_name('home').clear()
+        wd.find_element_by_name('home').send_keys(Contacts.home)
+
+        wd.find_element_by_name('submit').click()
+        self.return_contact_page()
 
     def return_contact_page(self):
         wd = self.app.wd
+
+        # Return to groups page
+        wd.find_element_by_css_selector('img[title="Addressbook"]').click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_css_selector('img[title="Edit"]').click()
+
+        wd.find_element_by_css_selector('input[value="Delete"]').click()
+
+        self.return_contact_page()
--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -3,5 +3,10 @@ from model.contact import Contacts
 
 def test_add_contact(app):
     app.session.login(user='admin', password='secret')
-    # app.contact.create(Contacts(lastname='', firstname='', nickname='', middlename=''))
+    app.contact.create(Contacts(lastname='123', firstname='123', home='777777777'))
+    app.session.logout()
+
+def test_delete_contact(app):
+    app.session.login(user='admin', password='secret')
+    app.contact.delete_first_contact()
     app.session.logout()
