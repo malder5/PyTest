8b65e5c81e03e2db6f51c9df174c003d9e3afffb
Roman <mulyarchik.rk@yandex.ru>
2020-04-25 17:37:35 +0300

    Добавлен тест. Выделен класс Выделены вспомогательные методы.

---
+++ group.py
@@ -0,0 +1,9 @@
+
+
+class group:
+    def __init__(self,
+                 name,
+                 header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
