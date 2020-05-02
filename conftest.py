22e1dd0be596508c25d7c6a8cebd3193e5a1492d
Roman <mulyarchik.rk@yandex.ru>
2020-05-02 19:20:45 +0300

    Часть внешних опций вынесено во внешний конфигурационный файл

--- conftest.py
+++ conftest.py
@@ -1,16 +1,23 @@
 import pytest
+import json
+import os
 from fixture.application import Application
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
+    browser = request.config.getoption('--browser')
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption('--target'))
+        with open(config_file, encoding='utf8') as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
-        browser = request.config.getoption('--browser')
-        base_url = request.config.getoption('--base_url')
-        fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username='admin', password='secret')
+        fixture = Application(browser=browser, base_url=target['base_url'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 @pytest.fixture(scope="session", autouse = True)
@@ -24,4 +31,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action = 'store', default='chrome')
-    parser.addoption('--base_url', action = 'store', default='http://localhost/addressbook/')
+    parser.addoption('--target', action = 'store', default='target.json')
